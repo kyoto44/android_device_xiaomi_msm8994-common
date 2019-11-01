edf6750d8758093eca8102e61668d80d13ec9b3c
kyoto44 <kyoto1337@protonmail.com>
2019-11-01 22:29:09 +0500

    fix typo and camera props

--- system_prop.mk
+++ system_prop.mk
@@ -287,3 +287,13 @@ ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=2048
 
+# Additional buffers shared between Camera and Video
+vidc.enc.dcvs.extra-buff-count=2
+
+#low power mode for camera
+vendor.camera.lowpower.record.enable=1
+
+persist.camera.stats.test=5
+#properties for limiting preview size in camera
+persist.vendor.camera.display.umax=1920x1080
+persist.vendor.camera.display.lmax=1280x720
