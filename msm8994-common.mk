98e7f876669407acaaa46130de78f0f756eb6b64
Dmitry Gomzyakov <nicerok11@gmail.com>
2019-11-20 22:27:02 +0500

    msm8994-common: bring back Vibrator HAL

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
