096e41a2d91924b7ac6f247a494edf3d115972dc
Dmitry Gomzyakov <nicerok11@gmail.com>
2019-11-20 17:25:46 +0500

    msm8994-common: remove vibrator HAL

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -244,11 +244,6 @@ PRODUCT_PACKAGES += \
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
     
 # Wifi
 PRODUCT_PACKAGES += \
