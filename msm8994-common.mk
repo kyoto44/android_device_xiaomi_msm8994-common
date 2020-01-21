6d99469a85f2ecf1c474257554a5e127d87704fe
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-01-21 21:37:19 +0500

    Revert "msm8994-common: [DNM] Disable updatable APEX"
    This reverts commit 99798d693c3ace0ca8dcbf7639017dc9b421a6c6.

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
