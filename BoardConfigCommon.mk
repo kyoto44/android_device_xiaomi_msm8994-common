c4c6c85004e1b9168cbb8d3a581955f0fd212eee
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-02-01 23:40:15 +0500

    msm8994-common: use gcc 9.2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,7 +96,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := /home/kyoto44/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := /home/kyoto44/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Partition
@@ -212,9 +212,5 @@ ifeq ($(WITH_TWRP),true)
 RECOVERY_VARIANT := twrp
 endif
 
-
 # Build
 BUILD_BROKEN_DUP_RULES := true
-
-# SurfaceFlinger
-TARGET_USE_AOSP_SURFACEFLINGER := true
